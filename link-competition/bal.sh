13f197e962d3e15967ac75dfd9b65ca4478ff9ce
audzms2776 <delkkate96@gmail.com>
2018-12-23 12:59:42 +0900

    Create bal.sh

---
+++ link-competition/bal.sh
@@ -0,0 +1,6 @@
+tbears balance hxbfd31645a404f133241892215b27ba5f4d5e7202
+tbears balance hx79568e346607a1325f3ea2c0b2fbf66b9f1feee3
+tbears balance hx8e4962e15db83ea9ea73b55b9589adaaaf7641c5
+tbears balance hx73b3785d79c953055efb36bbaf47f4fb1a0c58f4
+tbears balance hxb5681689d8e13a296bbec84d8042eee9f868e7e7
+
